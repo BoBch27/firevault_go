c831dc096a0b4cf4d60f3a6576894005472d0d4e
Bobby Donev <donev.bobby@gmail.com>
2025-03-26 19:19:04 +0000

    Added support for val funcs with only field scope param

--- validator_adaptors.go
+++ validator_adaptors.go
@@ -8,13 +8,28 @@ type ValidationFunc interface {
 	toValFuncInternal() valFuncInternal
 }
 
+// A ValFunc is a function that's executed
+// during a field validation.
+type ValFunc func(fs FieldScope) (bool, error)
+
+// turns exported func type to internal val func type
+func (v ValFunc) toValFuncInternal() valFuncInternal {
+	if v == nil {
+		return nil
+	}
+
+	return func(_ context.Context, fs FieldScope) (bool, error) {
+		return v(fs)
+	}
+}
+
 // A ValFuncCtx is a context-aware function
 // that's executed during a field validation.
 // Useful when a validation may depend
 // dynamically on a context.
 type ValFuncCtx func(ctx context.Context, fs FieldScope) (bool, error)
 
-// implement method to satisfy interface
+// turns exported func type to internal val func type
 func (v ValFuncCtx) toValFuncInternal() valFuncInternal {
 	if v == nil {
 		return nil
